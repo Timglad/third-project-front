2baeafa8ea57dafe4cc21c2d1798e8491baf79eb
ran <ran@goz.com>
2023-01-18 18:38:07 +0200

    adding pictures

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ function Product({product}) {
         <h1>{product.name}</h1>
         <h3>{product.description}</h3>
         <h3>{product.price}</h3>
-        <img src={'http://127.0.0.1:8000/static'+product.image} alt={product.name}/>
+        <img src={'https://shopping-k6qe.onrender.com'+product.image} alt={product.name}/>
     </div>
   )
 }
