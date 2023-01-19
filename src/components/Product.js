e305d8d752bc84f25602911157fa134f464f8ce9
ran <ran@goz.com>
2023-01-19 10:47:28 +0200

    added static to image folder url

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ function Product({product}) {
         <h1>{product.name}</h1>
         <h3>{product.description}</h3>
         <h3>{product.price}</h3>
-        <img src={'https://shopping-k6qe.onrender.com'+product.image} alt={product.name}/>
+        <img src={'https://shopping-k6qe.onrender.com/static'+product.image} alt={product.name}/>
     </div>
   )
 }
