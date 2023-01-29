8cac70deeac9b6f6f287c43e4f2381d682f6aa38
ran <ran@goz.com>
2023-01-29 15:56:39 +0200

    preventing form from being sent and updating real user pass

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,11 @@
 function LoginPage({login}){
-    return (
+    function formlogin(e){
+        e.preventDefault()
+        login(e.target.username.value, e.target.password.value)
+    }
+return (
         <div>
-            <form onSubmit={login("ran","1234")}>
+            <form onSubmit={formlogin}>
                 <input type="text" name="username" placeholder="Enter Username" />
                 <input type="password" name="password" placeholder="Enter Password" />
                 <input type="submit"/>
